a48adac45cbe072b0b9e7a60b0d101f469cc73d0
hiarica07 <halilarica@gmail.com>
2024-09-04 16:54:28 +0300

    I copied whole project from my private react folder. I'll continue from there 04-09-2024 date

---
+++ src/components/MenuListItems.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import { useNavigate } from 'react-router-dom';
+
+
+const links = [    // DRY  Do Not Repeat Yourself
+    {
+        title:"Dashboard",
+        url:"/stock"
+    },
+    {
+        title:"Firms",
+        url:"/stock/firms"
+    }
+]
+
+const MenuListItems = () => {
+    const navigate = useNavigate()
+  return (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {links.map((item, index) => (    
+          <ListItem key={item.title} disablePadding>
+            <ListItemButton onClick={()=>navigate(item.url)}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={item.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        
+      </List>
+    </div>
+  )
+}
+
+export default MenuListItems
